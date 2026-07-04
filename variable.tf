a44c406991805d96ecab5b74a554433c0d0e1fb5
MaheshDayaka <85336574+MaheshDayaka@users.noreply.github.com>
2026-07-04 16:23:35 +0530

    Remove default values from variables
    Removed default values for several variables in variable.tf.

--- variable.tf
+++ variable.tf
@@ -1,30 +1,24 @@
 variable "instancename" {
   description = "this is name of the instance"
   type = string
-  default = "terraform new"
 }
 variable "inttype" {
   description = "this is name of the instance type"
   type = string
-  default = "t3.micro"
 }
 variable "amiid" {
   description = "Name of the AMI Image ID"
   type = string
-  default = "ami-06067086cf86c58e6"
 }
 variable "key" {
   description = "name of the key pair"
   type = string
-  default = "mar-keypair"
 }
 variable "vsize" {
   description = "volume size"
   type = number
-  default = 10
 }
 variable "Azone" {
   description = "availability sone"
   type = string
-  default = "us-east-1a"
 }
